1857abfe36ff08c31141554b33913ef97d5262c4
looking <looking.faier@hotmail.com>
2018-12-22 13:41:35 +0800

    interface dll generate project renamed

--- interface_practice/Class.cpp
+++ interface_practice/Class.cpp
@@ -2,3 +2,22 @@
 
 
 
+
+std::string derivedClass::getName() const	//不会修改class member 或调用 非const member function
+{
+	return _name;
+}
+
+int derivedClass::getID() const
+{
+	return _id;
+}
+
+
+my_INTERFACE bool getObject(void **pp_obj)
+{
+	baseClass *pdc = new derivedClass();
+	*pp_obj = (void*)pdc;
+
+	return true;
+}
--- interface_practice/Class.h
+++ interface_practice/Class.h
@@ -44,22 +44,3 @@ class derivedClass : public baseClass
 	INTERFACE_DERIVED	//每个member function 仍要手动实现
 };
 
-
-std::string derivedClass::getName() const	//不会修改class member 或调用 非const member function
-{
-	return _name;
-}
-
-int derivedClass::getID() const
-{
-	return _id;
-}
-
-
-my_INTERFACE bool getObject(void **pp_obj)
-{
-	baseClass *pdc = new derivedClass();
-	*pp_obj = (void*)pdc;
-
-	return true;
-}
